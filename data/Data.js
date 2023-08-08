2b2c0242982375270f1cd451231bb17dede22eb0
Cleossa <cleossacleoo@gmail.com>
2023-08-08 20:22:35 +0700

    first commit

---
+++ data/Data.js
@@ -0,0 +1,20 @@
+export const imageSlider = [
+    "https://images.unsplash.com/photo-1483985988355-763728e1935b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvcHBpbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    "https://media.istockphoto.com/photos/tourist-woman-with-backpack-rear-view-picture-id1212702893?b=1&k=20&m=1212702893&s=170667a&w=0&h=0H3fStHp4Cspdyyd0Mrv2P-OT-bVxSh_CPIggBFAWPA=",
+    "https://media.istockphoto.com/photos/portrait-of-a-man-sitting-with-a-suit-with-a-watch-studio-picture-id909869956?b=1&k=20&m=909869956&s=170667a&w=0&h=aCTsGeeDq_aK6__fWA32qPzOcXls9f2fYTEn_cJwCvg=",
+    "https://images.unsplash.com/photo-1598343175492-9e7dc0e63cc6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8c2xpcHBlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1523380744952-b7e00e6e2ffa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGNhcHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+];
+
+export const categoryList = [
+    {
+        id: 1,
+        name: 'FPS Games',
+        icon: 'https://img.freepik.com/free-photo/old-military-officer-pointing-with-gun_651396-1691.jpg?w=740&t=st=1691500083~exp=1691500683~hmac=92ff1d3d775b30783647cd944dca966da57901fc5cc94a4f7fd7890f164a5144'
+    },
+    {
+        id: 2,
+        name: 'Android Games',
+        icon: 'https://img.freepik.com/free-vector/mobile-phone-shiny-background_1406-18.jpg?w=740&t=st=1691499307~exp=1691499907~hmac=0741fe94cf8a3a4d254a3f3ad6e1b479dd88b92dc35933e85425e3382c082b3e'
+    }
+];
---
+++ data/GameData.js
@@ -0,0 +1,41 @@
+export const GameData  = [
+    {
+        id: 1,
+        name : 'Valorant',
+        icon: require('../assets/images/VALO.png'),
+        genre : 'Tactical shooter'
+    },
+    {
+        id: 2,
+        name : 'CS:GO',
+        icon: require('../assets/images/CSGO.png'),
+        genre : 'Tactical shooter'
+    },
+    {
+        id: 3,
+        name : 'War Thunder',
+        icon: require('../assets/images/war thunder.jpg'),
+        genre : 'Combat flight simulator'
+    },
+    {
+        id: 4,
+        name : 'Apex legends',
+        icon: require('../assets/images/apex.png'),
+        genre : 'Battle royale game (FPS) '
+    },
+    {
+        id: 5,
+        name : 'ManEater',
+        icon: require('../assets/images/maneater.jpg'),
+        genre : 'Shark adventure game'
+    },
+   
+]
+
+export const imageSlider = [
+"https://images.wallpapersden.com/image/download/sova-s-valorant_bGZmaGmUmZqaraWkpJRobWllrWdma2U.jpg",
+'https://static.warthunder.com/upload/image/!%202020%20NEWS/09%20September/Raining%20Fire/1920x1080_logo_raining_fire_nanchang_q5_eng_fd853632ca64632329b65a1748290dae.jpg',
+'https://mmo-obzor.ru/_pu/4/81334777.jpg',
+'https://1.bp.blogspot.com/-8XXEDY6Mh6s/XfLwkwBJQ-I/AAAAAAAAHDA/9WBwpBkI5c0QRZMGLOapRRLCdzSQ3Il-gCNcBGAsYHQ/w1200-h630-p-k-no-nu/maneater-pc-protocoloxp-4.jpg',
+'https://4.bp.blogspot.com/-KcGLXqSvTaU/W1tph9NHTkI/AAAAAAAAPFA/ScxN0GgIGr8UjAzPwHKaMitZGoqAUk7GgCLcBGAs/s1600/Free%2BFire.png'
+]
